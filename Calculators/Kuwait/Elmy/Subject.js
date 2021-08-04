ed3d91e41be5c69b947975d920f59cd88715ec78
Gamal09 <88214478+Gamal09@users.noreply.github.com>
2021-08-04 16:00:15 +0200

    Delete Calculators directory

--- Calculators/Kuwait/Elmy/Subject.js
+++
@@ -1,9 +0,0 @@
-﻿//Subject class
-function Subject(sCode, sName, sGrade, sMark, sStatus, cNumber) {
-    this.subjectCode = sCode;
-    this.subjectName = sName;
-    this.subjectGrade = sGrade;
-    this.subjectMark = sMark;
-    this.subjectStatus = sStatus;
-    this.conditionNumber = cNumber;
-}
